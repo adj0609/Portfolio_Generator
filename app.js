2705b936fa1b24f7c75c7dddff264ac73cfbbd9e
Andrea Jared <adj0803@gmail.com>
2022-08-11 17:17:42 -0400

    html

--- app.js
+++ app.js
@@ -1,16 +1,12 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 
-const printProfileData = profileDataArr => {
-    for (let i = 0; i < profileDataArr.length; i += 1) {
-    console.log(profileDataArr[i]);
-    }
+const profileDataArgs = process.argv.slice(2);
+const name = profileDataArgs[0];
+const github = profileDataArgs[1];
 
-    console.log('===============');
+  fs.writeFile('./index.html', generatePage('name', 'github'), err => {
+    if (err) throw Error(err);
 
-    profileDataArr.forEach((profileItem) => {
-        console.log(profileItem)
-    });
-  };
-  
-  printProfileData(profileDataArgs);
+    console.log('Portfolio Complete! Check out index.html to see the output!');
+  });
