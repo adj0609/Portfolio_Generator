c55bb26fd88dd2183564f95e9eeb9f3642840dbd
Andrea Jared <adj0803@gmail.com>
2022-08-11 20:00:44 -0400

    inquirer

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
+const inquirer = require('inquirer');
+//const fs = require('fs');
+//const generatePage = require('./src/page-template');
+//
+//const pageHTML = generatePage('name', 'github');
+//
+//fs.writeFile('./index.html', pageHTML,  err => {
+//    if (err) throw err;
+//
+//    console.log('Portfolio Complete! Check out index.html to see the output!');
+//  });
 
-const profileDataArgs = process.argv.slice(2);
-const name = profileDataArgs[0];
-const github = profileDataArgs[1];
+const promptUser =() => {
+    return inquirer.prompt([
+        {
+        Type: 'input',
+        name: 'name',
+        message: 'What is your name?'
+        }
+    ]);
+};
 
-  fs.writeFile('./index.html', generatePage('name', 'github'), err => {
-    if (err) throw Error(err);
-
-    console.log('Portfolio Complete! Check out index.html to see the output!');
-  });
+promptUser().then(answers => console.log(answers));
