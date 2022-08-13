c875a07e0f266d8c3ef30f22b46994b489d3d0dd
Andrea Jared <adj0803@gmail.com>
2022-08-13 12:16:03 -0400

    commit

--- app.js
+++ app.js
@@ -16,8 +16,72 @@ const promptUser =() => {
         Type: 'input',
         name: 'name',
         message: 'What is your name?'
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'What is your GitHub username?'
+        },
+        {
+            type: 'input',
+            name: 'about',
+            message: 'Provide some information about you'
+        }
+    ]);  
+};
+
+const promptProject = portfolioData => {
+    if (!portfolioData.projects) {
+        portfolioData.projects =[];
+    }
+    console.log(`
+    ==============
+    Add a New Project
+    ==============
+    `);
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is the name of your project?'
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Provide a description of the project (Required)',
+        },
+        {
+            type: 'input',
+            name: 'link',
+            message: 'Enter the Github link to your project. (Required)'
+        },
+        {
+            type: 'confirm',
+            name: 'feature',
+            message: 'Would you like to feature this project?',
+            default: false
+        },
+        {
+            type: 'confirm',
+            name: 'confirmAddProject',
+            message: 'Would you like to add another project?',
+            default: false
         }
     ]);
 };
 
-promptUser().then(answers => console.log(answers));
+promptUser()
+.then(promptProject)
+.then(portfolioData => {
+    console.log(portfolioData);
+})
+.then(promptProject)
+.then(projectAnswers => console.log(projectAnswers))
+.then(projectData => {
+    portfolioData.projects.push(projectData);
+    if (projectData.confirmAddProject) {
+        return promptProject(portfolioData);
+        } else {
+            return portfolioData;
+    }
+});
